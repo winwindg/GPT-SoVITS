24c4e492c0a7389cf8f45d7b6451fc078f06fd62
Carver Gu <carver.gu@qq.com>
2024-12-05 15:11:04 +0800

    init project

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# kill previous process of web_ui.py
-ps aux | grep '[w]eb_ui.py' | awk '{print $2}' | xargs -r kill -9
+# kill previous process of webui.py
+ps aux | grep '[w]ebui.py' | awk '{print $2}' | xargs -r kill -9
 
 # get and enter script directory
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 cd $SCRIPT_DIR
 
-# activate conda environment and run web_ui.py
+# activate conda environment and run webui.py
 source activate gpt_sovits
-nohup python web_ui.py > app.log 2>&1 &
+nohup python webui.py > app.log 2>&1 &
